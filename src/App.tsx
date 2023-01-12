a8ac6e5096aa92273d3feda0a3a672b4141f11b0
Malinda Anupama <55244908+malinda666@users.noreply.github.com>
2023-01-13 01:07:54 +0530

    add lenis and gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
+
 const App = () => {
-  return <div className='container mx-auto'>app</div>
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: 'vertical', // vertical, horizontal
+      gestureDirection: 'vertical', // vertical, horizontal, both
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false
+    })
+
+    //get scroll value
+    // lenis.on('scroll', (values: any) => {
+    //   console.log(values?.scroll)
+    // })
+
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+  return <div className='container mx-auto h-[4000vh] text-[40vh]'>app</div>
 }
 
 export default App
